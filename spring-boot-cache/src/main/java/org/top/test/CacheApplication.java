ccf970ff88219db7a13cfa042615969cfd8efda8
lisi <lisi@163.com>
2024-04-22 14:00:18 +0800

    redis第一次Stringdemo

---
+++ spring-boot-cache/src/main/java/org/top/test/CacheApplication.java
@@ -0,0 +1,14 @@
+package org.top.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JXS
+ */
+@SpringBootApplication
+public class CacheApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CacheApplication.class,args);
+    }
+}
